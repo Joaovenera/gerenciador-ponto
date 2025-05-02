f5bf2c631763f936e585b24a6c72125863e45540
Joaovenera <joaootavio.v@gmail.com>
2025-05-02 04:24:57 -0300

    allowedHosts config

--- server/vite.ts
+++ server/vite.ts
@@ -25,7 +25,7 @@ export async function setupVite(app: Express, server: Server) {
     hmr: { server },
     allowedHosts: [
       "localhost",
-      "jarmaq.com.br", // leading dot allows subdomains
+      ".jarmaq.com.br", // leading dot allows subdomains
     ],
   };
 
